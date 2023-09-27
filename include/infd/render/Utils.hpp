2fc4b2777a02340f31a52adf9abfbbb4dccbe3eb
Crispin <crispinha@users.noreply.github.com>
2023-09-27 14:49:54 +1300

    Add fullscreen mesh builder

---
+++ include/infd/render/Utils.hpp
@@ -0,0 +1,12 @@
+//
+// Created by crispin on 27/09/23.
+//
+
+#pragma once
+
+#include <infd/GLMesh.hpp>
+#include <glm/glm.hpp>
+
+namespace infd::render {
+    GLMesh build_fullscreen_texture_mesh();
+}
---
+++ src/infd/render/Utils.cpp
@@ -0,0 +1,23 @@
+//
+// Created by crispin on 27/09/23.
+//
+#include "infd/render/Utils.hpp"
+
+namespace infd::render {
+    GLMesh build_fullscreen_texture_mesh() {
+        GLMeshBuilder mb;
+
+        mb.vertices = {
+                {glm::vec3{0, 0, 0}, glm::vec3{0}, glm::vec2{0}},
+                {glm::vec3{0, 1, 0}, glm::vec3{0}, glm::vec2{0}},
+                {glm::vec3{1, 1, 0}, glm::vec3{0}, glm::vec2{0}},
+                {glm::vec3{1, 0, 0}, glm::vec3{0}, glm::vec2{0}}
+        };
+        mb.indices = {
+                0, 1, 2,
+                2, 3, 0
+        };
+
+        return mb.build();
+    }
+}
