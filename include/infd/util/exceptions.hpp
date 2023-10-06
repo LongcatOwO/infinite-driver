7bf4dbdb061d5eb598021cc8f1174fe7624dbc23
Phuwasate Lutchanont <lutchaphuw@ecs.vuw.ac.nz>
2023-10-06 14:31:43 +1300

    Added more exceptions

--- include/infd/util/exceptions.hpp
+++ include/infd/util/exceptions.hpp
@@ -25,4 +25,28 @@ namespace infd::util {
 		NullPointerException(const std::string &what_arg) : std::runtime_error(what_arg) {}
 		NullPointerException(const char *what_arg) : std::runtime_error(what_arg) {}
 	};
+
+	class ExternalLibraryException : public std::runtime_error {
+	public:
+		ExternalLibraryException(const std::string &what_arg) : std::runtime_error(what_arg) {}
+		ExternalLibraryException(const char *what_arg) : std::runtime_error(what_arg) {}
+	};
+
+	class GLFWException : public ExternalLibraryException {
+	public:
+		GLFWException(const std::string &what_arg) : ExternalLibraryException(what_arg) {}
+		GLFWException(const char *what_arg) : ExternalLibraryException(what_arg) {}
+	};
+
+	class GLEWException : public ExternalLibraryException {
+	public:
+		GLEWException(const std::string &what_arg) : ExternalLibraryException(what_arg) {}
+		GLEWException(const char *what_arg) : ExternalLibraryException(what_arg) {}
+	};
+
+	class ImGuiException : public ExternalLibraryException {
+	public:
+		ImGuiException(const std::string &what_arg) : ExternalLibraryException(what_arg) {}
+		ImGuiException(const char *what_arg) : ExternalLibraryException(what_arg) {}
+	};
 } // namespace infd::scene
