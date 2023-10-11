651876e96f6c11e371915e6ce678164bb7824a29
Anfri Hayward <haywarmatt@ecs.vuw.ac.nz>
2023-10-11 22:53:40 +1300

    Resolve "Implement basic perlin mesh generation for chunks"

--- src/infd/generator/ChunkPtr.cpp
+++ src/infd/generator/ChunkPtr.cpp
@@ -1,11 +1,28 @@
 #include "infd/generator/ChunkPtr.hpp"
 
+#include "infd/generator/meshbuilding/PerlinMesh.hpp"
+#include "infd/render/RenderComponent.hpp"
+#include "infd/Wavefront.hpp"
+
 namespace infd::generator {
-    ChunkPtr::ChunkPtr(ChunkGenerator& generator) {
-        //TODO
+    ChunkPtr::ChunkPtr(scene::Component &parent, render::Renderer &renderer, ChunkGenerator &generator) :
+            ChunkPtr(parent.sceneObject(), renderer, generator) {}
+
+    ChunkPtr::ChunkPtr(scene::SceneObject& scene, render::Renderer& renderer, ChunkGenerator& generator) {
+        scene::SceneObject& chunkSceneObject = scene.addChild((std::stringstream() << "Chunk: " << generator.x << ", "<< generator.y).str());
+
+        auto& mesh = chunkSceneObject.emplaceComponent<render::RenderComponent>(
+                renderer,
+                meshbuilding::generatePerlinMesh(generator.x, generator.y, generator.perlinNoise)
+        );
+
+        mesh.transform().localPosition(glm::vec3(generator.x, 0, generator.y));
+
+        _chunkScenePointer = &chunkSceneObject;
     }
 
     void ChunkPtr::detach() {
+        (void)_chunkScenePointer->removeFromParent();
         _detached = true;
     }
 }
